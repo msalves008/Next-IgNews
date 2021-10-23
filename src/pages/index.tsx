4bb93b6e132dd6da073a8a1bb9ccceec3d33b72d
msalves@008 <matheusdesousaalves2018@gmail.com>
2021-10-23 14:14:09 -0300

    create next project, initial configs, and global styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head><title>Ig News</title></Head>
+      <h1>      
+        Hello World!
+      </h1>
+    </>
+  )
+}
